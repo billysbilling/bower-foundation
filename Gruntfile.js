dd50973c7059fc50e5fd70d6694f403fa94a0430
Sebastian Seilund <sebastian@seilund.com>
2013-11-01 16:00:28 -0700

    added foundation

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-        billy_builder: {
-            title: 'bower-foundation'
-        }
-    });
-
-    grunt.loadNpmTasks('billy-builder');
-};
